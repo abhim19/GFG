5b8732890fc0e537cfca9d9723c502b879648954
Admin <abhimahindrakar19@gmail.com>
2025-01-31 19:56:12 +0530

    Session 4 JS Basics

---
+++ Session4/js-1/script.js
@@ -0,0 +1,84 @@
+// var a = "abhi";
+// var b = 23;
+// var c = true;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// function calculate1(input) {
+//   console.log(input);
+// }
+
+// calculate1(10);
+
+// let, const
+// var - global scope
+// let, const - block scoped
+
+//conditions
+// var a = 10;
+// if (a < 5) {
+//   console.log("Greater");
+// } else if (a == 10) {
+//   console.log("Equal");
+// } else {
+//   console.log("Lower");
+// }
+
+//Array
+// const fruits = ["Apple", "Mango", 1, 2];
+// console.log(fruits);
+
+// fruits[0] = "Orange";
+// console.log(fruits);
+
+//Objects
+// const person = {
+//   fName: "Abhi",
+//   lName: "Mahindrakar",
+//   age: 24,
+//   required: false,
+//   sports: ["Football", "Cricket"],
+// };
+// console.log(person);
+
+//hoisting
+// x = 10;
+// console.log(x);
+// var x;
+
+//Function hoisting
+// printOut();
+// function printOut() {
+//   console.log("Printing...");
+// }
+
+//arrow functions
+// const printOut = () => {
+//   console.log("Printing.....");
+// };
+// printOut();
+
+//IIFE functions - Immediately Invoked Function Expression
+
+// (() => {
+//   console.log("Printing.....");
+// })();
+
+//Loops
+
+// for (let x = 0; x < 10; x += 2) {
+//   console.log(x);
+// }
+
+// let x = 0;
+// while (x < 10) {
+//   console.log(x);
+//   x++;
+// }
+
+// do {
+//   console.log(x);
+//   x++;
+// } while (x > 10);
