13d0776058aeb8f0c95e7d04dfd0ad2b6af5b6ac
Abhishek M <abhimahindrakar19@gmail.com>
2025-02-19 15:01:43 +0530

    simple changes

--- Session8/second-project/src/App.jsx
+++ Session8/second-project/src/App.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react ";
 
 const App = () => {
-  function handleClick() {
-    console.log("Clicked");
+  let name = "";
+  function handleClick(event) {
+    name = event.target.value;
+    console.log("Name", name);
   }
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World - {name}</h1>
       <button onClick={handleClick}>Submit</button>
+      <input
+        type="text"
+        placeholder="Enter name"
+        onChange={(e) => {
+          name = e.target.value;
+        }}
+      />
     </div>
   );
 };
